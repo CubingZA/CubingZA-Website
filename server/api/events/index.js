0a0dcd05aff552770b222581d5a718a477d4816b
AlphaSheep <brendan@alphasheep.co.za>
2023-04-07 10:16:48 +0200

    Clean up of competition routes

--- server/api/events/index.js
+++ server/api/events/index.js
@@ -5,13 +5,13 @@ import * as auth from '../../auth/auth.service';
 var router = express.Router();
 
 router.get('/', controller.index);
-router.get('/:id', controller.show);
-router.get('/upcoming', controller.upcoming);
 router.post('/', auth.hasRole('admin'), controller.create);
-router.post('/:id/notify', auth.hasRole('admin'), controller.sendNotifications);
-router.delete('/', auth.hasRole('admin'), controller.destroy);
+router.get('/upcoming', controller.upcoming);
+
+router.get('/:id', controller.show);
 router.put('/:id', auth.hasRole('admin'), controller.upsert);
-router.patch('/:id', auth.hasRole('admin'), controller.patch);
 router.delete('/:id', auth.hasRole('admin'), controller.destroy);
+
+router.post('/:id/notify', auth.hasRole('admin'), controller.sendNotifications);
 
 export default router;
