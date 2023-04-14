ee4c9f9dc2434230abc474867072c375d64a3a15
AlphaSheep <brendan@alphasheep.co.za>
2023-04-14 21:36:29 +0200

    Fix some front end tests

--- client/src/app/account/login/login.component.spec.ts
+++ client/src/app/account/login/login.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -8,6 +8,7 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
--- client/src/app/services/competition/competition.service.spec.ts
+++ client/src/app/services/competition/competition.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CompetitionService } from './competition.service';
 
 describe('CompetitionService', () => {
   let service: CompetitionService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(CompetitionService);
   });
 
