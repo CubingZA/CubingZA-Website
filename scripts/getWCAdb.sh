6d8ef00ec6d1f09358b18352b2e6abf8eadbc132
AlphaSheep <brendan@alphasheep.co.za>
2023-04-29 17:33:47 +0200

    Update ports to avoid conflicts

--- scripts/getWCAdb.sh
+++ scripts/getWCAdb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -O ~/Downloads/WCA_export.sql.zip https://www.worldcubeassociation.org/results/misc/WCA_export.sql.zip 
+wget -O ~/Downloads/WCA_export.sql.zip https://www.worldcubeassociation.org/results/misc/WCA_export.sql.zip
 unzip -o ~/Downloads/WCA_export.sql.zip -d ~/WCADatabase/
-mysql --port=3333 --user=wca -password=wca wca < ~/WCADatabase/WCA_export.sql
+mysql --port=4204 --user=wca -password=wca wca < ~/WCADatabase/WCA_export.sql
 
