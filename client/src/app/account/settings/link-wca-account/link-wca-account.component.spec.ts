756a269624f4250d974989a936f262751f266d11
AlphaSheep <brendan@alphasheep.co.za>
2023-05-16 21:21:26 +0200

    Fix smoke test for link WCA account component

--- client/src/app/account/settings/link-wca-account/link-wca-account.component.spec.ts
+++ client/src/app/account/settings/link-wca-account/link-wca-account.component.spec.ts
@@ -1,17 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LinkWcaAccountComponent } from './link-wca-account.component';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { AlertsService } from 'src/app/components/alerts/alerts.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 describe('LinkWcaAccountComponent', () => {
   let component: LinkWcaAccountComponent;
   let fixture: ComponentFixture<LinkWcaAccountComponent>;
 
+  let authService: jasmine.SpyObj<AuthService>;
+  let alerts: jasmine.SpyObj<AlertsService>;
+
   beforeEach(() => {
+    const authServiceSpy = jasmine.createSpyObj('AuthService', [
+      'isLocalUser', 'hasVerifiedEmail', 'isWCAUser'
+    ]);
+    const alertsSpy = jasmine.createSpyObj('AlertsService', [
+      'clear'
+    ]);
+
     TestBed.configureTestingModule({
-      declarations: [LinkWcaAccountComponent]
+      imports: [FontAwesomeModule],
+      declarations: [LinkWcaAccountComponent],
+      providers: [
+        { provide: AuthService, useValue: authServiceSpy },
+        { provide: AlertsService, useValue: alertsSpy }
+      ]
     });
     fixture = TestBed.createComponent(LinkWcaAccountComponent);
     component = fixture.componentInstance;
+
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
+    alerts = TestBed.inject(AlertsService) as jasmine.SpyObj<AlertsService>;
+
     fixture.detectChanges();
   });
 
