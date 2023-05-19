b77cf976b8cf97d99cc349266018021ad2be960c
AlphaSheep <brendan@alphasheep.co.za>
2023-05-19 19:49:12 +0200

    Add Terms of Use

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -7,11 +7,13 @@ import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotificationsComponent } from './pages/notifications/notifications.component';
 import { PrivacyComponent } from './pages/privacy/privacy.component';
+import { TermsComponent } from './pages/terms/terms.component';
 
 export const AppRoutes: Routes = [
   { path: '', component: HomeComponent, title: 'CubingZA - Home' },
   { path: 'about', component: AboutComponent, title: 'CubingZA - About' },
   { path: 'privacy', component: PrivacyComponent, title: 'CubingZA - Privacy Policy' },
+  { path: 'terms', component: TermsComponent, title: 'CubingZA - Terms of Use' },
   { path: 'notifications', canActivate: [AuthGuard], component: NotificationsComponent, title: 'CubingZA - Notifications' },
   { path: 'contact', component: ContactComponent, title: 'CubingZA - Contact Us' },
   ...AccountRoutes,
@@ -21,5 +23,5 @@ export const AppRoutes: Routes = [
     canMatch: [AdminGuard]
   }
 
-  
+
 ];
