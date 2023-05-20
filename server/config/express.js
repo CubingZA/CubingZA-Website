cabb5a734a5a019db81e3d0d2bf163111aaf0f17
Brendan Gray <brendan@alphasheep.co.za>
2023-05-20 18:30:37 +0200

    Add maxAge to session cookies

--- server/config/express.js
+++ server/config/express.js
@@ -33,6 +33,7 @@ export default function(app) {
     cookie: {
       secure: false, // In production this should be overridden at at the nginx layer using, for example, "proxy_cookie_flags ~ secure;"
       sameSite: 'lax', // Needed for OAuth to work
+      maxAge: 21600000 // 6 hours in milliseconds
     },
     store: MongoStore.create({
       mongoUrl: config.mongoUrl,
