2b09c75df09c99b12a1c6180a60b08810256015f
AlphaSheep <brendan@alphasheep.co.za>
2023-05-22 20:55:53 +0200

    Add rankings API route

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,6 @@
 import contact from './api/contact';
 import records from './api/records';
+import rankings from './api/rankings';
 import events from './api/events';
 import emails from './api/emails';
 import users from './api/users';
@@ -8,6 +9,7 @@ import auth from './auth';
 export default function routes(app) {
   app.use('/api/contact', contact);
   app.use('/api/records', records);
+  app.use('/api/rankings', rankings);
   app.use('/api/events', events);
   app.use('/api/emails', emails);
   app.use('/api/users', users);
