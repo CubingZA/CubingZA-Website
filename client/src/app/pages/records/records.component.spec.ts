49e40e378f53874d606f26f0bd1aca7dda7d44e7
AlphaSheep <brendan@alphasheep.co.za>
2023-06-11 09:37:09 +0200

    Add a provincial records component

---
+++ client/src/app/pages/records/records.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecordsComponent } from './records.component';
+
+describe('RecordsComponent', () => {
+  let component: RecordsComponent;
+  let fixture: ComponentFixture<RecordsComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [RecordsComponent]
+    });
+    fixture = TestBed.createComponent(RecordsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- client/src/app/services/record/record.service.ts
+++ client/src/app/services/record/record.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, tap } from 'rxjs';
+import { Observable, catchError, map, tap } from 'rxjs';
+import { Ranking } from '../rankings/rankings.service';
+import { EventsService } from '../events/events.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +10,7 @@ import { catchError, tap } from 'rxjs';
 export class RecordService {
 
   constructor(
+    private eventsService: EventsService,
     private http: HttpClient
   ) { }
 
@@ -23,6 +26,36 @@ export class RecordService {
       })
     );
   }
+
+  getPronvincialRecords(): Observable<ProvincialRecordTable> {
+    return this.http.get<ProvincialRecordResponse>('/api/rankings/records')
+    .pipe(
+      map((rankings) => this.convertRecordResponseToRecordTable(rankings)),
+    );
+  }
+
+  convertRecordResponseToRecordTable(response: ProvincialRecordResponse): ProvincialRecordTable {
+    let records: ProvincialRecordTable = {};
+    Object.keys(response).forEach((eventId) => {
+      records[eventId] = [];
+      Object.keys(response[eventId]).forEach((province) => {
+        let ranking = response[eventId][province];
+        let record: Record = {
+          eventName: this.eventsService.getEventName(eventId),
+          eventId: eventId,
+          singleName: ranking.single?.personName ? ranking.single.personName : "",
+          singleResult: ranking.single?.best ? ranking.single.best : "",
+          singleId: ranking.single?.wcaID ? ranking.single.wcaID : "",
+          averageName: ranking.average?.personName ? ranking.average.personName : "",
+          averageResult: ranking.average?.best ? ranking.average.best : "",
+          averageId: ranking.average?.wcaID ? ranking.average.wcaID : "",
+          province: province
+        };
+        records[eventId].push(record);
+      });
+    });
+    return records;
+  }
 }
 
 export type Record = {
@@ -37,4 +70,19 @@ export type Record = {
   averageId: string;
   averageDate?: Date;
   eventRank?: number;
+  province?: string;
 };
+
+export type ProvincialRecordTable = {
+  [eventId: string]: Record[];
+}
+
+type ProvincialRecordResponse = {
+  [eventId: string]: {
+    [province: string]: {
+      single: Ranking;
+      average: Ranking;
+    }
+  }
+}
+
