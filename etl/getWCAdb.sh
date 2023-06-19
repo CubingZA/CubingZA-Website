0028c2eb3650ebdb77cbe50a61d8dd5360526fea
Brendan Gray <brendan@alphasheep.co.za>
2023-06-19 18:31:22 +0200

    Fix WCA results download

--- etl/getWCAdb.sh
+++ etl/getWCAdb.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+# Prepare a temporary directory
+TMPFOLDER=/tmp/wca-database
+mkdir -p $TMPFOLDER
+
 # Download and extract the latest WCA database
-wget -O ~/Downloads/WCA_export.sql.zip https://www.worldcubeassociation.org/results/misc/WCA_export.sql.zip
-unzip -o ~/Downloads/WCA_export.sql.zip -d ~/WCADatabase/
+wget -O $TMPFOLDER/WCA_export.sql.zip https://www.worldcubeassociation.org/export/results/WCA_export.sql.zip
+unzip -o $TMPFOLDER/WCA_export.sql.zip -d $TMPFOLDER
 
 # Copy the database to the docker container
-docker cp ~/WCADatabase/WCA_export.sql cubingza-website-wca-database-1:/tmp/WCA_export.sql
+docker cp $TMPFOLDER/WCA_export.sql cubingza-website-wca-database-1:/tmp/WCA_export.sql
 
 # Import the database into the docker container
 docker exec cubingza-website-wca-database-1 sh -c 'mysql --user=wca --password=wca wca < /tmp/WCA_export.sql'
-
-
-
