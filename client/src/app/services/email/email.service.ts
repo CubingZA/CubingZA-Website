a8b4ac63e9d4822e2a2c309a30a61232f9b476d6
AlphaSheep <brendan@alphasheep.co.za>
2023-06-27 21:35:14 +0200

    Add EmailMessage interface

--- client/src/app/services/email/email.service.ts
+++ client/src/app/services/email/email.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { EmailMessage } from 'src/app/interfaces/email/email-message';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +13,3 @@ export class EmailService {
     return this.http.post('/api/contact/send', message);
   }
 }
-
-export type EmailMessage = {
-  name: string;
-  email: string;
-  subject: string;
-  message: string;
-};
