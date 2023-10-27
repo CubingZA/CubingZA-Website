7363430ca4401a21142acf22bb9b43b97f4fd302
AlphaSheep <brendan@alphasheep.co.za>
2023-10-27 20:32:05 +0200

    Update record model to allow for ties

--- client/src/app/interfaces/record/record.ts
+++ client/src/app/interfaces/record/record.ts
@@ -1,16 +1,19 @@
 export interface Record {
   eventName: string;
   eventId: string;
-  singleName: string;
-  singleResult: string;
-  singleId: string;
-  singleDate?: Date;
-  singleNR?: boolean;
-  averageName: string;
-  averageResult: string;
-  averageId: string;
-  averageDate?: Date;
-  averageNR?: boolean;
   eventRank?: number;
+
   province?: string;
+
+  singleResult: string;
+  singleName: string[];
+  singleId: string[];
+  singleDate?: Date[];
+  singleNR?: boolean;
+
+  averageResult: string;
+  averageName: string[];
+  averageId: string[];
+  averageDate?: Date[];
+  averageNR?: boolean;
 }
