f548dcc20505119836e76adeb8edae9aa295f1d5
elfishhenry <109970614+elfishhenry@users.noreply.github.com>
2024-08-12 19:57:42 +0200

    Faqs ts component.

---
+++ client/src/app/pages/faqs/faqs.component.spec.ts
@@ -0,0 +1,19 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { faqsComponent } from './faqs.component';
+
+describe('faqsComponent', () => {
+  let component: faqsComponent;
+  let fixture: ComponentFixture<faqsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ faqsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(faqsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+});
