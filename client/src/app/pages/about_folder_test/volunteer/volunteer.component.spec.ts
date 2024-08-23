9aa9a25772624404c275d881c21282808f2d66a6
elfishhenry <109970614+elfishhenry@users.noreply.github.com>
2024-08-23 14:42:19 +0200

    Volunteering components added into the new dropdown.

---
+++ client/src/app/pages/about_folder_test/volunteer/volunteer.component.spec.ts
@@ -0,0 +1,9 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VolunteerComponent } from './volunteer.component';
+
+describe('VolunteerComponent', () => {
+  it('should create', () => {
+    expect(new VolunteerComponent()).toBeTruthy(); 
+  });
+});
---
+++ client/src/app/pages/about_folder_test/volunteer/volunteer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-volunteer',
+  templateUrl: './volunteer.component.html',
+})
+export class VolunteerComponent {
+}
