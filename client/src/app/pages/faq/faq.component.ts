bd07378b8dcb22296e7c944cc0d45464ae9c6653
elfishhenry <109970614+elfishhenry@users.noreply.github.com>
2024-08-23 14:42:41 +0200

    Got rid of old component files for the new dropdown component files.

--- client/src/app/pages/faq/faq.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-Faq',
-  templateUrl: './faq.component.html',
-})
-export class FaqComponent {
-}
--- client/src/app/pages/volunteer/volunteer.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-volunteer',
-  templateUrl: './volunteer.component.html',
-})
-export class VolunteerComponent {
-}
