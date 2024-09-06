e45fd3b5baa4c0474daf5f379aa66d30eab0bfca
elfishhenry <109970614+elfishhenry@users.noreply.github.com>
2024-09-06 14:53:38 +0200

    Fixed the file location error,

--- server/components/notificationEmailer/notificationEmailer.js
+++ server/components/notificationEmailer/notificationEmailer.js
@@ -17,7 +17,7 @@ export default function sendNotificationEmails(comp) {
       if (user.notificationSettings[province] && user.role !== 'unverified') {
         // User has notification settings turned on for this province
 
-        fs.readFile('/Users/elfis/Documents/code/CubingZA-Website/server/components/notificationEmailer/notification.template.html', 'utf8', (err, data) => {
+        fs.readFile('./components/notificationEmailer/notification.template.html', 'utf8', (err, data) => {
           if (err) {
             console.error("Failed to read email template:", err);
             return;
